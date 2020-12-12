0b9c45d5e67c4707ab9a5fa3d404410fd195603d
Martin Simov <martinsimov02@gmail.com>
2020-12-12 15:28:37 +0200

    Added basic password change form.

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import PassowrdChaneForm from '../PasswordChange';
 
 const AccountPage = () => (
     <div>
         <h1>Account Page</h1>
+        <PassowrdChaneForm/>
     </div>
 );
 
