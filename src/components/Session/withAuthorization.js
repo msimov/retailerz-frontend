05768e0f62573f11c391b7517af5a7beeac85da0
Martin Simov <martinsimov02@gmail.com>
2020-12-26 17:08:54 +0200

    Removed bad import and useless return in withAuthorization

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -12,7 +12,6 @@ const withAuthorization = (condition) => Component => {
                 authUser => {
                     if (!condition(authUser)) {
                         this.props.history.push(ROUTES.SIGN_IN);
-                        return
                     }
                 }
             );
