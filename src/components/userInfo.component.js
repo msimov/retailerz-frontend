6f96339ca67bfdf41958431c3a2c8b455e331d05
Martin Simov <martin.simov@microinvest.net>
2021-02-10 13:42:16 +0200

    Added design for user info

--- src/components/userInfo.component.js
+++ src/components/userInfo.component.js
@@ -1,34 +1,55 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { formatURL } from '../commons/url.common';
 import UserService from '../services/user.service';
 import { FirebaseContext } from "../context/firebase.context";
+import { Card, Icon, Image } from 'semantic-ui-react';
 
 const UserInfo = ({match}) => {
     const firebase = useContext(FirebaseContext);
     const currentUser = firebase.getCurrentUser();
-    const url = formatURL(match.url);
-    const {userId} = match.params;
     const [user, setUser] = useState(null);
 
     useEffect(() => {
         currentUser.getIdToken().then(idToken => {
-            UserService.findByUserId(userId, idToken).then(res => {
+            UserService.findByUserId(currentUser.uid, idToken).then(res => {
+                console.log(res)
                 setUser(res);
             })
         })
-    }, [currentUser, userId]);
+    }, [currentUser]);
 
 
     return user ? ( 
-        <div>        
+        <Card>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>{user.userFirstName} {user.userLastName}</Card.Header>
+                <Card.Meta>
+                    <span>{currentUser.email}</span>
+                </Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+                <Link to={`/users/${currentUser.uid}/edit`}>
+                    <Icon name='edit'/>
+                    Edit Profile
+                </Link>
+            </Card.Content>
+            <Card.Content extra>
+                <Link to={`/users/${currentUser.uid}/change-password`}>
+                    <Icon name='lock'/>
+                    Change password
+                </Link>
+            </Card.Content>
+        </Card>
+
+/*         <div>        
             {user.userId}
             {user.userFirstName}
             {user.userLastName}
             {user.userEmail}
             <Link to={`${url}/edit`}>Edit Profile</Link>
             <Link to={`${url}/change-password`}>Change Password</Link>
-        </div>
+        </div> */
     )
     : (
         <div>Loading...</div>
