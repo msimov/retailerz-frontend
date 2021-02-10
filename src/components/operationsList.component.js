d5aa3dcd8a6693fe10eaf3ee87db9cde13d56326
Martin Simov <martinsimov02@gmail.com>
2021-02-10 19:55:15 +0200

    Added design for operations list

--- src/components/operationsList.component.js
+++ src/components/operationsList.component.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { formatURL } from '../commons/url.common';
 import OperationService from '../services/operation.service';
 import { FirebaseContext } from "../context/firebase.context";
+import { Button, Menu, Table } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const OperationsList = ({match}) => {
     const firebase = useContext(FirebaseContext);
     
-    const url = formatURL(match.url);
-    const {userId} = match.params; 
     const [operations, setOperations] = useState(null);
     const currentUser = firebase.getCurrentUser();
 
 
     useEffect(() => {
         currentUser.getIdToken().then(idToken => {
-            OperationService.getAllByUserId(userId, idToken).then(res => {
+            OperationService.getAllByUserId(currentUser.uid, idToken).then(res => {
                 setOperations(res);
             })
         })
-    }, [currentUser, userId]);
+    }, [currentUser]);
 
     const deleteOperation = (operationId) => {
         setOperations(operations.map(operation => {
@@ -36,7 +34,52 @@ const OperationsList = ({match}) => {
     }
 
     return(
-        <div>
+
+        <Table celled>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>Type</Table.HeaderCell>
+                    <Table.HeaderCell>Store</Table.HeaderCell>
+                    <Table.HeaderCell>Product</Table.HeaderCell>
+                    <Table.HeaderCell>Quantity</Table.HeaderCell>
+                    <Table.HeaderCell>Price</Table.HeaderCell>
+                    <Table.HeaderCell>Actions</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+                {operations && operations.map(operation => {
+                    console.log(operation)
+                    return(
+                        <Table.Row key={operation.operationId}>
+                            <Table.Cell>{operation.operationTypeName}</Table.Cell>
+                            <Table.Cell>{operation.storeName}</Table.Cell>
+                            <Table.Cell>{operation.productName}</Table.Cell>
+                            <Table.Cell>{operation.operationCount} {operation.measureUnitName}</Table.Cell>
+                            <Table.Cell>{operation.operationCount * operation.productRetailPrice} USD</Table.Cell>
+                            <Table.Cell>
+                            <Menu className='ui bottom attached' widths='2'>
+                                <Menu.Item
+                                    as={Link}
+                                    to={`/users/${operation.operationUserId}/operations/${operation.operationId}/edit`}
+                                >
+                                    Edit
+                                </Menu.Item>
+                                <Menu.Item 
+                                    as={Button}
+                                    onClick={() => deleteOperation(operation.operationId)} disabled={operation.isDeleting}
+                                >
+                                    Delete
+                                </Menu.Item>
+                            </Menu>
+                            </Table.Cell>
+                        </Table.Row>
+                    )
+                })}
+            </Table.Body>
+        </Table>
+
+        /*         <div>
             <h1>Operations</h1>
             <Link to={`${url}/add`}>Add Operation</Link>
             {operations && operations.map(operation =>
@@ -57,7 +100,7 @@ const OperationsList = ({match}) => {
                 <div>No Operations To Display</div>
             }
 
-        </div>
+        </div> */
     );
 }
 
